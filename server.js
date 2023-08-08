680e73e90d3cbdb44a61d83efe53681d14f88d03
varshahv15 <varshahv15@gmail.com>
2023-08-08 14:10:23 +1000

    MongoDB package
    -Installed mongodb package
    -Used DBConnection
    -Posted images

--- server.js
+++ server.js
@@ -1,22 +1,62 @@
 let express = require('express');
 let app = express ();
 let port = process.env.port || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://varshahv15:Hima15@cluster0.gnraqup.mongodb.net/?retryWrites=true&w=majority";
+let collection;
 
 app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req,res)=>{
-    res.render('index.html');
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function runDBConnection() {
+  try {
+      await client.connect();
+      collection = client.db().collection('Cat');
+      console.log(collection);
+  } catch(ex) {
+      console.error(ex);
+  }
+}
+
+app.get('/', function (req,res) {
+  res.render('index.html');
 });
 
-app.get('/addingTwoNumbers', (req,res)=>{
-    let number1 = req.query.Number1;
-    let number2 = req.query.Number2;
-    let SumOfNumbers = parseInt(number1) + parseInt(number2);
-    let obj = {statusCode:200, message:'Success', data:SumOfNumbers};
+app.get('/api/cats', (req,res) => {
+  getAllCats((err,result)=>{
+      if (!err) {
+          res.json({statusCode:200, data:result, message:'get all cats successful'});
+      }
+  });
+});
 
-    res.json(obj);
-})
+app.post('/api/cat', (req,res)=>{
+  let cat = req.body;
+  postCat(cat, (err, result) => {
+      if (!err) {
+          res.json({statusCode:201, data:result, message:'success'});
+      }
+  });
+});
+
+function postCat(cat,callback) {
+  collection.insertOne(cat,callback);
+}
+
+function getAllCats(callback){
+  collection.find({}).toArray(callback);
+}
+
 app.listen(port, ()=>{
-    console.log('server is started');
-
+  console.log('express server started');
+  runDBConnection();
 });
